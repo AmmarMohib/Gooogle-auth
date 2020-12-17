7ae36af6b358c6a0fc0436aad8e50775a3c6f77b
AmmarMohib <66912469+AmmarMohib@users.noreply.github.com>
2020-12-17 16:59:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// let signup = () => {
+//    let email = document.getElementById('email')
+// let password = document.getElementById('password')
+// firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+//   .then((result) => {
+//     console.log(result)
+//   })
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     console.log(errorMessage)
+//   });
+// }
 let signin = () => {
 var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -6,7 +19,33 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   console.log(result)
   // The signed-in user info.
   var user = result.user;
+  let pic = result.additionalUserInfo.profile.picture
+  let nme = result.additionalUserInfo.profile.name; 
+  document.getElementById('image').src = pic
+  document.getElementById('hea').innerHTML = nme
   // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(error)
+  // ...
+});
+}
+function gui() {
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+  console.log(result.user.additionalUserInfo)
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
@@ -17,3 +56,5 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   var credential = error.credential;
   // ...
 });
+
+}
